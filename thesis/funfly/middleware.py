5a6166d74d0214b146c14eef0af5036958135e97
Rustin Alexandru <rustinalexandru@gmail.com>
2016-06-14 00:11:17 +0300

    Removed social auth

--- thesis/funfly/middleware.py
+++ thesis/funfly/middleware.py
@@ -1,8 +1,7 @@
 class TimezoneMiddleware(object):
 
     def process_request(self, request):
-        pass
-        # if request.user.is_authenticated():
-        #     timezone.activate(pytz.timezone(request.user.userprofile.timezone))
-        # else:
-        #     timezone.deactivate()
+        if request.user.is_authenticated():
+            timezone.activate(pytz.timezone(request.user.userprofile.timezone))
+        else:
+            timezone.deactivate()
