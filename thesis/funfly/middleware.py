d4f2a60c9c95b954ed90aac10654c434dfac6cf5
Rustin Alexandru <rustinalexandru@gmail.com>
2016-06-14 00:12:33 +0300

    Fixed middleware

--- thesis/funfly/middleware.py
+++ thesis/funfly/middleware.py
@@ -1,6 +1,11 @@
+import pytz
+from django.utils import timezone
+
+
 class TimezoneMiddleware(object):
 
     def process_request(self, request):
+        pass
         if request.user.is_authenticated():
             timezone.activate(pytz.timezone(request.user.userprofile.timezone))
         else:
